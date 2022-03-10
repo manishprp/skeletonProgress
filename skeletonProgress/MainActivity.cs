28654d9d2129247d5eea66678b8832f3c2b41c39
manish.rajpurohit <mrpurohit80055@gmail.com>
2022-03-10 11:53:56 +0530

    Add project files.

---
+++ skeletonProgress/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Supercharge;
+using AndroidX.AppCompat.App;
+
+namespace skeletonProgress
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity        
+    {
+       ShimmerLayout shimmerLayout;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            shimmerLayout = FindViewById<ShimmerLayout>(Resource.Id.shimmerLayout);
+          shimmerLayout.StartShimmerAnimation();
+
+
+        }
+
+    }
+}
